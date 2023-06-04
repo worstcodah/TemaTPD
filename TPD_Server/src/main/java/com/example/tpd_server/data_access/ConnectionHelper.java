449450cf16d429e5e7ab3d8563cb9e4424d8e7c4
Rafa <rafael4296@gmail.com>
2023-06-04 14:03:20 +0300

    Update4.

--- TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql://34.116.151.224/tema-orm:europe-central2:tema-orm-db:postgres?user=postgres&password=postgres";
+    private static final String url = "jdbc:postgresql://34.116.151.224/tema-orm:europe-central2:tema-orm-db/postgres?user=postgres&password=postgres";
 
     public static Connection getConnection() {
         Connection connection = null;
