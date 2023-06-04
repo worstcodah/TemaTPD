97568e15748de8652e9aa52dbe3c9cefb800c216
Rafa <rafael4296@gmail.com>
2023-06-04 15:00:26 +0300

    Update9.

--- TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,15 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql://34.116.151.224:5432/tema-orm:europe-central2:tema-orm-db/postgres";
-    private static final String user = "postgres";
-    private static final String password = "postgres";
+    private static final String url = "jdbc:postgresql://34.116.151.224:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=tema-orm:europe-central2:tema-orm-db&user=postgres&password=postgres";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(ConnectionHelper.url, ConnectionHelper.user, ConnectionHelper.password);
+            connection = DriverManager.getConnection(ConnectionHelper.url);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
