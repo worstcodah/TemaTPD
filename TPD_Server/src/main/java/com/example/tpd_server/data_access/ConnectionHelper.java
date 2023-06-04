ee70b0676864abece9b26c9ff69207b52565f8d0
Rafa <rafael4296@gmail.com>
2023-06-04 16:54:03 +0300

    Update15.

--- TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql://34.116.151.224:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=tema-orm:europe-central2:tema-orm-db&user=postgres&password=postgres&sslmode=require&sslrootcert=/opt/glassfish6/glassfish/domains/domain1/docroot/certificate.crt";
+    private static final String url = "jdbc:postgresql://34.116.151.224:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=tema-orm:europe-central2:tema-orm-db&user=postgres&password=postgres";
 
     public static Connection getConnection() {
         Connection connection = null;
